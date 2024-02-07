d3756da6125d239032fdd0c7bdc5b01d5d3bbe9a
Vinay Kumar Chelpuri <chelpuri.v@northeastern.edu>
2024-02-07 10:19:46 -0500

    work flow changes

--- src/main/java/com/cloudnativewebapp/webapp/WebAppApplication.java
+++ src/main/java/com/cloudnativewebapp/webapp/WebAppApplication.java
@@ -1,11 +1,8 @@
 package com.cloudnativewebapp.webapp;
 
 import com.cloudnativewebapp.webapp.Service.DatabaseService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class WebAppApplication {
