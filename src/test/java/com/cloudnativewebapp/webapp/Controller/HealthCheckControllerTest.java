539bc02232cdc77be385530da8dd04404cf20828
Vinay Kumar Chelpuri <chelpuri.v@northeastern.edu>
2024-02-11 22:00:05 -0500

    added integration tests using restassured

--- src/test/java/com/cloudnativewebapp/webapp/Controller/HealthCheckControllerTest.java
+++ src/test/java/com/cloudnativewebapp/webapp/Controller/HealthCheckControllerTest.java
@@ -1,58 +1,58 @@
-package com.cloudnativewebapp.webapp.Controller;
-
-import com.cloudnativewebapp.webapp.Service.HealthCheckService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@WebMvcTest(HealthCheckController.class)
-class HealthCheckControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private HealthCheckService healthCheckService;
-
-    @BeforeEach
-    void setUp() {
-        Mockito.when(healthCheckService.isDBConnected()).thenReturn(true);
-    }
-
-//    @Test
-//    void checkDBHealth() throws Exception {
-//        mockMvc.perform(MockMvcRequestBuilders.get("http://localhost:8080/healthz"))
-//                .andExpect(status().isOk());
+//package com.cloudnativewebapp.webapp.Controller;
 //
-//        mockMvc.perform(MockMvcRequestBuilders.get("/healthz?1=1"))
-//                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+//import com.cloudnativewebapp.webapp.Service.HealthCheckService;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.Mockito;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@WebMvcTest(HealthCheckController.class)
+//class HealthCheckControllerTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @MockBean
+//    private HealthCheckService healthCheckService;
+//
+//    @BeforeEach
+//    void setUp() {
+//        Mockito.when(healthCheckService.isDBConnected()).thenReturn(true);
 //    }
 //
-//    @Test
-//    void putDBHealth() throws Exception {
-//        mockMvc.perform(MockMvcRequestBuilders.put("/healthz"))
-//                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
-//    }
-//
-//    @Test
-//    void postDBHealth() throws Exception {
-//        mockMvc.perform(MockMvcRequestBuilders.post("/healthz"))
-//                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
-//    }
-//
-//    @Test
-//    void deleteDBHealth() throws Exception {
-//        mockMvc.perform(MockMvcRequestBuilders.delete("/healthz"))
-//                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
-//    }
-}
+////    @Test
+////    void checkDBHealth() throws Exception {
+////        mockMvc.perform(MockMvcRequestBuilders.get("http://localhost:8080/healthz"))
+////                .andExpect(status().isOk());
+////
+////        mockMvc.perform(MockMvcRequestBuilders.get("/healthz?1=1"))
+////                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+////    }
+////
+////    @Test
+////    void putDBHealth() throws Exception {
+////        mockMvc.perform(MockMvcRequestBuilders.put("/healthz"))
+////                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+////    }
+////
+////    @Test
+////    void postDBHealth() throws Exception {
+////        mockMvc.perform(MockMvcRequestBuilders.post("/healthz"))
+////                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+////    }
+////
+////    @Test
+////    void deleteDBHealth() throws Exception {
+////        mockMvc.perform(MockMvcRequestBuilders.delete("/healthz"))
+////                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+////    }
+//}
