004c4b8d4fdb19c9653f68092d406c4143ae4a73
Vinay Kumar Chelpuri <chelpuri.v@northeastern.edu>
2024-02-18 20:14:26 -0500

    gmi script changes

--- packer/gmi-script.sh
+++ packer/gmi-script.sh
@@ -13,11 +13,11 @@ sudo sed -i '82 s/ident/md5/' /var/lib/pgsql/data/pg_hba.conf
 sudo systemctl restart postgresql
 sudo dnf install java-17-openjdk-devel -y
 sudo alternatives --set java /usr/lib/jvm/java-17-openjdk-17.0.6.0.9-0.3.ea.el8.x86_64/bin/java
-sudo dnf install maven -y
-echo 'export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-17.0.6.0.9-0.3.ea.el8.x86_64/' >> ~/.bashrc
-echo 'export M2_HOME=/opt/maven' >> ~/.bashrc
-echo 'export PATH=${M2_HOME}/bin:${PATH}' >> ~/.bashrc
-source ~/.bashrc
-mvn -version
+#sudo dnf install maven -y
+#echo 'export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-17.0.6.0.9-0.3.ea.el8.x86_64/' >> ~/.bashrc
+#echo 'export M2_HOME=/opt/maven' >> ~/.bashrc
+#echo 'export PATH=${M2_HOME}/bin:${PATH}' >> ~/.bashrc
+#source ~/.bashrc
+#mvn -version
 java -version
 
