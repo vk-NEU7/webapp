53b18f2e56a228ebd09297dc2e4a434f1ad49253
Vinay Kumar Chelpuri <chelpuri.v@northeastern.edu>
2024-02-24 23:56:43 -0500

    workflow and application properties changes

--- src/main/java/com/cloudnativewebapp/webapp/WebAppApplication.java
+++ src/main/java/com/cloudnativewebapp/webapp/WebAppApplication.java
@@ -1,6 +1,5 @@
 package com.cloudnativewebapp.webapp;
 
-import com.cloudnativewebapp.webapp.Service.DatabaseService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebAppApplication {
 
 	public static void main(String[] args) {
-		DatabaseService dbService = new DatabaseService();
-		dbService.createDatabase();
 		SpringApplication.run(WebAppApplication.class, args);
 	}
 
