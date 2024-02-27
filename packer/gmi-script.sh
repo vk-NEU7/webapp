77361cfc1b78f2bb3e8e3216f4c69e7b5880ea09
Vinay Kumar Chelpuri <chelpuri.v@northeastern.edu>
2024-02-27 16:20:11 -0500

    Script added application properties command

--- packer/gmi-script.sh
+++ packer/gmi-script.sh
@@ -4,6 +4,10 @@ sudo groupadd csye6225
 sudo useradd -s /usr/sbin/nologin -g csye6225 -d /opt/webapp -m csye6225
 sudo mv /tmp/web-app-0.0.1-SNAPSHOT.jar /opt/webapp/
 sudo mv /tmp/webapp.service /etc/systemd/system/
+touch /tmp/application.properties
+sudo mv /tmp/application.properties /opt/webapp/
+sudo chown csye6225:csye6225 /opt/webapp/application.properties
+sudo chmod 750 /opt/webapp/application.properties
 sudo dnf install java-17-openjdk-devel -y
 sudo alternatives --set java /usr/lib/jvm/java-17-openjdk-17.0.6.0.9-0.3.ea.el8.x86_64/bin/java
 #sudo dnf install maven -y
